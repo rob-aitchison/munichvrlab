db349ac4ad576d3780d841c9037e97cacd6c6e0e
Rob Aitchison <43424110+rob-aitchison@users.noreply.github.com>
2020-03-18 19:55:50 -0400

    Fixes to properly add the env argument and change naming

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ class MunichVRLab(Application):
         self.engine.register_command("augmented_cmd", self.augmented, params)
 
         params2 = {
-            "title": "Collaboration",
+            "title": "Tiguan Collab",
         }
 
         self.engine.register_command("collaboration_cmd", self.collaboration, params2)
 
         params3 = {
-            "title": "Powerwall",
+            "title": "Open with VARJO",
         }
 
         self.engine.register_command("powerwall_cmd", self.powerwall, params3)
@@ -51,14 +51,17 @@ class MunichVRLab(Application):
         software_launcher = sgtk.platform.create_engine_launcher(tk, context, "tk-vred")
         software_versions = software_launcher.scan_software()
         launch_info = software_launcher.prepare_launch(software_versions[-1].path, "")
-        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'load(\'//10.146.20.210/Demo/VRED/Augmented/SRG_Augmented_V9.vpb\')'])
+        env = os.environ.copy()
+        for k in launch_info.environment:
+            env[k] = launch_info.environment[k]
+        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'load(\'//10.146.20.210/Demo/VRED/Augmented/SRG_Augmented_V9.vpb\')'], env=env)
 
         self.engine.log_info("Augmented Script Launched")
 
 
     def collaboration(self, entity_type, entity_ids):
         # this message will be displayed to the user
-        self.engine.log_info("Collaboration Script Requested")
+        self.engine.log_info("Tiguan Collab Script Requested")
 
         chosen_file = self.shotgun.find_one(entity_type, [["id", "is", entity_ids[0]]], ["path", "task", "entity"])
         tk = sgtk.sgtk_from_path(chosen_file["path"]["local_path_windows"])
@@ -67,14 +70,19 @@ class MunichVRLab(Application):
         software_launcher = sgtk.platform.create_engine_launcher(tk, context, "tk-vred")
         software_versions = software_launcher.scan_software()
         launch_info = software_launcher.prepare_launch(software_versions[-1].path, "")
-        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'load(\'//10.146.20.210/Demo/VRED/Collaboration/Collab_2019_2_VW_Tiguan_35_VARJO.vpb\')'])
+        env = os.environ.copy()
+        for k in launch_info.environment:
+            env[k] = launch_info.environment[k]
+        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'load(\'//10.146.20.210/Demo/VRED/Collaboration/Collab_2019_2_VW_Tiguan_35_VARJO.vpb\')'], env=env)
+        filepath='C:/Batch/DEMO4_ps.bat'
+        subprocess.Popen(filepath, shell=True, stdout = subprocess.PIPE)
 
-        self.engine.log_info("Collaboration Script Launched")
+        self.engine.log_info("Tiguan Collab Script Launched")
 
 
     def powerwall(self, entity_type, entity_ids):
         # this message will be displayed to the user
-        self.engine.log_info("Powerwall Script Requested")
+        self.engine.log_info("Open with VARJO Script Requested")
 
         chosen_file = self.shotgun.find_one(entity_type, [["id", "is", entity_ids[0]]], ["path", "task", "entity"])
         tk = sgtk.sgtk_from_path(chosen_file["path"]["local_path_windows"])
@@ -83,6 +91,9 @@ class MunichVRLab(Application):
         software_launcher = sgtk.platform.create_engine_launcher(tk, context, "tk-vred")
         software_versions = software_launcher.scan_software()
         launch_info = software_launcher.prepare_launch(software_versions[-1].path, "")
-        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'load(\'//10.146.20.210/Demo/VRED/Powerwall/Tiguan_Powerwall_BG_4.vpb\'); selectVariantSet(\'Demo_Auto_Start\')'])
+        env = os.environ.copy()
+        for k in launch_info.environment:
+            env[k] = launch_info.environment[k]
+        subprocess.Popen([launch_info.path, launch_info.args, chosen_file["path"]["local_path_windows"], '-postpython', 'setDisplayMode(VR_DISPLAY_VARJO)'], env=env)
 
-        self.engine.log_info("Powerwall Script Launched")
+        self.engine.log_info("Open with VARJO Script Launched")
